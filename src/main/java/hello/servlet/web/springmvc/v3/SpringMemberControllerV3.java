64ccfa1661af80ebeb6bc2bddd8425f4bbc7808a
oooo91 <hyelneu29@gmail.com>
2023-08-25 14:08:12 +0900

    스프링 mvc - 실용적인 방식 (Http method 지정)

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,8 +4,8 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 /**
@@ -20,7 +20,8 @@ public class SpringMemberControllerV3 {
      * 1. ModelAndView 반환 안해도 된다.
      * v3 에서 v4로 개선된 것처럼 매번 ModelAndView를 반환하기 보다는 view name만 반환되도 그에 해당하는 뷰를 찾을 수 있도록 한다.
      */
-    @RequestMapping("/new-form")
+    //@RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String newForm() {
         return "new-form";
     }
@@ -31,7 +32,8 @@ public class SpringMemberControllerV3 {
      *  @RequestParam("username") 은 request.getParameter("username") 와 거의 같은 코드라 생각하면 된다.
      *  물론 GET 쿼리 파라미터, POST Form 방식을 모두 지원한다.
      */
-    @RequestMapping("/save")
+    //@RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
     public String memberSave(@RequestParam("username") String username,
                                    @RequestParam("age") int age,
                                    Model model) {
@@ -44,7 +46,8 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String members(Model model) {
 
         List<Member> members = memberRepository.findAll();
