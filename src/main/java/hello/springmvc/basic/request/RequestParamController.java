b702a227e16b3b160b110638d7cb7393c6a65017
oooo91 <hyelneu29@gmail.com>
2023-08-25 17:17:44 +0900

    HTTP 요청 파라미터 - @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,11 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -108,9 +106,59 @@ public class RequestParamController {
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         log.info("username={}, age={}",
-
                 paramMap.get("username"),
                 paramMap.get("age"));
         return "ok";
     }
+
+    /**
+     * 실제 개발을 하면 요청 파라미터를 받아서 필요한 객체를 만들고 그 객체에 값을 넣어주어야 한다. 보통 다음과 같이 코드를 작성할 것이다.
+     * @RequestParam String username;
+     * @RequestParam int age;
+     * HelloData data = new HelloData();
+     * data.setUsername(username);
+     * data.setAge(age);
+     * 스프링은 이 과정을 완전히 자동화해주는 @ModelAttribute 기능을 제공한다
+     * 참고: model.addAttribute(helloData) 코드도 함께 자동 적용됨, 뒤에 model을 설명할 때
+
+     * 스프링 MVC는 @ModelAttribute 가 있으면 다음을 실행한다.
+     * 1. HelloData 객체를 생성한다.
+     * 2. 요청 파라미터의 이름으로 HelloData 객체의 프로퍼티를 찾는다. 그리고 해당 프로퍼티의 setter를 호출해서 파라미터의 값을 입력(바인딩) 한다.
+     * 예) 파라미터 이름이 username 이면 setUsername() 메서드를 찾아서 호출하면서 값을 입력한다.
+
+     * 프로퍼티란?
+     * 객체에 getUsername() , setUsername() 메서드가 있으면, 이 객체는 username 이라는 프로퍼티를 가지고 있다고 한다. (getXXX -> xxx)
+     * username 프로퍼티의 값을 변경하면 setUsername() 이 호출되고, 조회하면 getUsername() 이 호출된다.
+     * class HelloData {
+     *  getUsername();
+     *  setUsername();
+     * }
+
+     * 바인딩 오류
+     * age=abc 처럼 숫자가 들어가야 할 곳에 문자를 넣으면 BindException 이 발생한다. 이런 바인딩 오류를 처리하는 방법은 검증 부분에서 다룬다.
+    */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}",
+                helloData.getUsername(),
+                helloData.getAge());
+        return "ok";
+    }
+
+    /**
+     * 1. @ModelAttribute 는 생략할 수 있다. (직접 만든 객체)
+     * 2. @RequestParam 도 생략할 수 있으니 혼란이 발생할 수는 있다.
+     * 3. 단 예외가 있는데 ArgumentResolver로 지정이 되어있는 그러니까 예약어 (예를 들어 HttpServletRequest) 같은 경우는
+     * 어노테이션이 없더라도 @ModelAttribute로 취급 안 한다.
+     * 4. argument resolver 로 지정해둔 타입 외 = @ModelAttribute
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(),
+                helloData.getAge());
+        return "ok";
+    }
+
 }
