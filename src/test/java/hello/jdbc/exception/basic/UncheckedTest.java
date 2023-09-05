332c81f96f1b4051b717e1799eb89d9daa679215
oooo91 <hyelneu29@gmail.com>
2023-09-05 17:44:09 +0900

    체크 예외 활용 -> 웬만해서 하지마

--- src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
+++ src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
@@ -39,12 +39,12 @@ public class UncheckedTest {
     @Test
     void unchecked_throw() {
         Service service = new Service();
-        Assertions.assertThatThrownBy(() -> service.callThrow()).isInstanceOf(MyUncheckecException.class);
+        Assertions.assertThatThrownBy(() -> service.callThrow()).isInstanceOf(MyUncheckedException.class);
     }
 
 
-    static class MyUncheckecException extends RuntimeException {
-        public MyUncheckecException(String message) {
+    static class MyUncheckedException extends RuntimeException {
+        public MyUncheckedException(String message) {
             super(message);
         }
     }
@@ -79,7 +79,7 @@ public class UncheckedTest {
      */
     static class Repository {
         public void call() {
-            throw new MyUncheckecException("ex");
+            throw new MyUncheckedException("ex");
         }
     }
 }
