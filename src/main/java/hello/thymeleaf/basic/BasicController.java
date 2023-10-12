1d456d9b7609bfcfc52182ff124f2f4e7121cfb5
oooo91 <hyelneu29@gmail.com>
2023-10-12 15:23:55 +0900

    블록

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -120,6 +120,11 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
 
     @Data
     static class User {
