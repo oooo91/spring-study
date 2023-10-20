f86c42387dbda1391a4a12dd3ae1bcef906948b6
oooo91 <hyelneu29@gmail.com>
2023-10-20 10:56:21 +0900

    html form의 불편한 점

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -39,6 +41,13 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        /**
+         체크박스를 체크하면 form에서 open=on 이라는 값이 넘어가는데 이때 스프링은 'on'이라는 문자를 'true' 타입으로 변환한다 (스프링 타입 컨버터)
+         문제는 체크박스를 언체크하면 아예 open이라는 필드가 넘어가지 않는다. item.getOpen=null 체크박스 특이다.
+         이 문제를 해결하기 위해 스프링이 지원하는 히든 언더바를 사용한다.
+         */
+        log.info("item.open={}", item.getOpen());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
