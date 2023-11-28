c14546f6cd522f7f5508f74a780abb30eac40115
oooo91 <hyelneu29@gmail.com>
2023-11-28 16:31:43 +0900

    DefaultHandlerExceptionResolver 추가

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -5,9 +5,12 @@ import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -41,10 +44,31 @@ public class ApiExceptionController {
   }
 
   //스프링 부트 -> 여러 exceptionResolver 제공
+
+  //ResponseStatusExceptionResolver -> http 응답 코드 변경
   //ResponseStatusExceptionResolver 가 @ResponseStatus 어노테이션 확인 -> response.sendError(code, reason) -> return ModelAndView();
   @GetMapping("/api/response-status-ex1")
   public String responseStatusEx1() {
     throw new BadRequestException();
+  }
+
+  //우리가 만든 예외가 아닌 우리가 수정할 수 없는 시스템 예외, 라이브러리 예외에는 @ResponseStatus 적용하기 어렵다.
+  //추가로 어노테이션을 사용하기 떄문에 조건에 따라 동적으로 변경하는 것도 어렵다.
+  //-> 이럴 때는 실제 RuntimeException 인 ResponseStatusException 을 터뜨리면 된다. (내가 넣을 오류 코드, 오류 메시지, 실제 예외)
+  //이 예외도 ResponseStatusExceptionResolver 얘가 처리한다.
+  @GetMapping("/api/response-status-ex2")
+  public String responseStatusEx2() {
+    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+  }
+
+  //DefaultHandlerExceptionResolver -> 스프링 내부 예외 처리
+  //DefaultHandlerExceptionResolver -> 얘가 알아서 클라이언트의 잘못은 500 에서 400 으로 변경해준다.
+  //가령 클라이언트에서 String 타입의 data 를 보냈다면 사실 클라이언트 잘못이다. 그런데 서버에서 문제가 발생했기 때문에 어쨌거나 스프링은 500을 터뜨린다.
+  //다만 defaultHandlerExceptionResolver 가 해당 오류를 보고 (TypeMismatchException) 이 오류에 관한 코드를 처리한다 (500 -> 400 변경)
+  //이러한 수많은 정의들이 defaultHandlerExceptionResolver 에 있다.
+  @GetMapping("/api/default-handler-ex")
+  public String defaultException(@RequestParam Integer data) {
+    return "ok";
   }
 
   @Data
--- src/main/java/hello/exception/exception/BadRequestException.java
+++ src/main/java/hello/exception/exception/BadRequestException.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 //ResponseStatusExceptionResolver 가 @ResponseStatus 어노테이션 확인 -> 클라이언트로 response.sendError(code, reason) , return ModelAndView();
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 요청 오류")
+//ResponseStatusExceptionResolver 가 reason 가져올 때 messageSource.getMessage 로 탐색하기 때문이다.
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")
 public class BadRequestException extends RuntimeException {
 
 }
