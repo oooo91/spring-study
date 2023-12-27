3ac6403fcdbcb80fdb1303d0d02aa402a849d89d
oooo91 <hyelneu29@gmail.com>
2023-12-27 20:52:29 +0900

    콜백

--- src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
+++ src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
@@ -8,6 +8,10 @@ public class ContextV2 {
 	/**
 	 * 전략을 필드가 아니라 파라미터로 받게 한다. (선 조립 후 실행)
 	 * 의존 관계를 미리 주입하는 게 아니라 전달하는 방식 (실행할 때 원하는 전략 유연하게 넣기)
+
+	 * 여기서 Strategy 는 콜백이다.
+	 * 콜백은 인수로 넘겨주는 실행 가능한 코드를 말한다. (그래서 자바스크립트에서 인수로 function 을 넘길 때 콜백이라고 하는 구나)
+	 * 왜 콜백이라고 이름 붙였냐? -> Strategy 라는 실행 가능한 코드가 뒤에서 즉, Context 에서 실행되기 떄문에 콜(실행된다) 백(뒤에서) 이라고 이름 붙였다.
 	 */
 	public void execute(Strategy strategy) {
 		long startTime = System.currentTimeMillis();
